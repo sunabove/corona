8b3870b5462df987092e0169141b2d2ac62da130
sunabove <sunabove@users.noreply.github.com>
2020-04-01 17:50:11 +0900

    **

---
+++ app/src/main/java/com/corona/Activity_03_CoronaList.java
@@ -0,0 +1,18 @@
+package com.corona;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_03_CoronaList extends ComActivity {
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_03__corona_list ;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
