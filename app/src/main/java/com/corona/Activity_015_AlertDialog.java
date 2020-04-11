36d8a3e270dc5459cae06d211692081c95be6e7a
sunabove <sunabove@users.noreply.github.com>
2020-04-11 09:18:29 +0900

    **

---
+++ app/src/main/java/com/corona/Activity_015_AlertDialog.java
@@ -0,0 +1,14 @@
+package com.corona;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_015_AlertDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_015_alert_dialog);
+    }
+}
